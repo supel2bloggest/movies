ff38d30f8c36de16ff4445c47a9a840e7843ec8e
Thanachai <thanakta@scg.com>
2023-06-01 01:19:25 +0700

    fix: error auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -15,11 +15,27 @@ const handler = NextAuth({
       },
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
-        if (credentials?.username === 'admin' && credentials?.password === 'admin') {
-          return Promise.resolve({
-            id: 9999,
-            username: "admin"
-          }) as any
+        const res = await fetch("http://localhost:3000/api/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: credentials?.username,
+            password: credentials?.password,
+          }),
+        });
+
+
+        const user = await res.json();
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
       },
     }),
--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const data = await request.json();
-  console.log('POST', data, request)
   if (data.username === "admin" && data.password === "admin") {
     return NextResponse.json({
       id: 9999,
